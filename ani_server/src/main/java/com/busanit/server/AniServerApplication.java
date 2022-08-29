433eaf592d80b7eeb7151cfbf9bbb465fccb272e
VisionMath <103171106+VisionMath@users.noreply.github.com>
2022-08-29 12:09:45 +0900

    220829 first

---
+++ ani_server/src/main/java/com/busanit/server/AniServerApplication.java
@@ -0,0 +1,13 @@
+package com.busanit.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniServerApplication.class, args);
+	}
+
+}
